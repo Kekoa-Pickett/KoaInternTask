2ab64ea0d3ab55efbe4873b58ab62ea4b60acde1
Koa Pickett <103221420+Kekoa-Pickett@users.noreply.github.com>
2024-07-05 13:07:30 -0700

    Getting rid of all extra info

--- qmake.py
+++ qmake.py
@@ -2,14 +2,12 @@ import sys
 import subprocess
 import os
 
-print("Start")
 os.makedirs(os.path.abspath("gen"), exist_ok=True)
-# pyside6-uic "C:\Users\KaliCajala\Documents\GitHub\kali-first-task\forms\main_window.ui" -o "C:\Users\KaliCajala\Documents\GitHub\kali-first-task\gen\ui_main_window.py"   
-# generate main_window.ui   
+# pyside6-uic "C:\Users\KoaPickett\Documents\GitHub\TaskTracker\forms\taskTrackerQT.ui" -o "C:\Users\KoaPickett\Documents\GitHub\TaskTracker\gen\taskTrackerQT_ui.py"   
+# generate taskTrackerQT_ui.py
 uic = "pyside6-uic.exe"
-# in_file = r"C:\Users\KaliCajala\Documents\GitHub\kali-first-task\forms\main_window.ui"
-# out_file = r"C:\Users\KaliCajala\Documents\GitHub\kali-first-task\gen\ui_main_window.py"
+# in_file = r"C:\Users\KoaPickett\Documents\GitHub\TaskTracker\forms\taskTrackerQT.ui"
+# out_file = r"C:\Users\KoaPickett\Documents\GitHub\TaskTracker\gen\taskTrackerQT_ui.py"
 in_file = os.path.abspath(r"forms\taskTrackerQT.ui")
 out_file = os.path.abspath(r"gen\taskTrackerQT_ui.py")
 subprocess.call([uic, "--from-imports", in_file, '-o', out_file], shell=True)
-print("End")
